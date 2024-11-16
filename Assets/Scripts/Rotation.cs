7ecd8cfcfe57cd40fea370c6e8125e81597f190f
Gwidaniel <danielmigwi.gits@gmail.com>
2024-11-16 11:52:57 +0300

    no message

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -4,46 +4,11 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
-    //Rotational Speed
     [SerializeField] float speed = 0f;
+    public Vector3 rotationAxis = Vector3.forward;
 
-    //Forward Direction
-    [SerializeField] bool ForwardX = false;
-    [SerializeField] bool ForwardY = false;
-    [SerializeField] bool ForwardZ = false;
-
-    //Reverse Direction
-    [SerializeField] bool ReverseX = false;
-    [SerializeField] bool ReverseY = false;
-    [SerializeField] bool ReverseZ = false;
-
-    void Update()
+    public void Update()
     {
-        //Forward Direction
-        if (ForwardX == true)
-        {
-            transform.Rotate(Time.deltaTime * speed, 0, 0, Space.Self);
-        }
-        if (ForwardY == true)
-        {
-            transform.Rotate(0, Time.deltaTime * speed, 0, Space.Self);
-        }
-        if (ForwardZ == true)
-        {
-            transform.Rotate(0, 0, Time.deltaTime * speed, Space.Self);
-        }
-        //Reverse Direction
-        if (ReverseX == true)
-        {
-            transform.Rotate(-Time.deltaTime * speed, 0, 0, Space.Self);
-        }
-        if (ReverseY == true)
-        {
-            transform.Rotate(0, -Time.deltaTime * speed, 0, Space.Self);
-        }
-        if (ReverseZ == true)
-        {
-            transform.Rotate(0, 0, -Time.deltaTime * speed, Space.Self);
-        }
+        transform.Rotate(rotationAxis * speed * Time.deltaTime);
     }
 }
